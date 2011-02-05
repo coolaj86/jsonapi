b9c828bfa215424547e504b3ad27f161e2876b1c
AJ ONeal <coolaj86@gmail.com>
2011-02-05 13:40:35 -0700

    more module cleanup

--- mkjsonapi.sh
+++ mkjsonapi.sh
@@ -3,7 +3,6 @@ cat \
   vendor/require-kiss/lib/require-kiss.js \
   vendor/global-es5.js \
   vendor/futures/lib/future.js \
-  vendor/url.js \
   vendor/ahr/lib/ahr.js \
   lib/jsonapi.js \
     > lib/jsonapi.all.js

de5e81236722b003e5e441bbca6525ae3ceed472
AJ ONeal <coolaj86@gmail.com>
2011-03-10 22:25:02 -0700

    added simple test

---
+++ test/require.js
@@ -0,0 +1,12 @@
+/*jslint onevar: true, undef: true, newcap: true, nomen: true, regexp: true, plusplus: true, bitwise: true, devel: true, debug: true, es5: true, maxerr: 10 */
+/*global module: true, exports: true, provide: true, clearTimeout: true, setTimeout: true, clearInterval: true, setInterval: true */
+// see  http://www.jslint.com/lint.html for meaning of above strings
+(function () {
+  "use strict";
+
+  require("require-kiss");
+
+  var jsonapi = require('../lib/jsonapi');
+
+  console.log(jsonapi.createRestClient);
+}());
